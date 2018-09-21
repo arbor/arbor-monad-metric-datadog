b090ed81f907ebc7d089b42f7e26ecacd318dab7
John Ky <newhoggy@gmail.com>
2018-09-21 16:22:29 +1000

    Remove arbor-monad-logger dependency

--- src/Arbor/Monad/Counter/Handle.hs
+++ src/Arbor/Monad/Counter/Handle.hs
@@ -14,11 +14,11 @@ module Arbor.Monad.Counter.Handle
   ) where
 
 import Arbor.Monad.Counter.Type     (CounterKey, CounterValue (CounterValue), Counters (Counters), CountersMap, MonadCounters)
-import Arbor.Monad.Logger
 import Control.Concurrent.STM.TVar
 import Control.Lens
 import Control.Monad
 import Control.Monad.IO.Class
+import Control.Monad.Logger
 import Control.Monad.STM            (STM, atomically)
 import Data.Foldable
 import Data.Generics.Product.Fields
@@ -72,7 +72,7 @@ logStats = do
   let nonzero = DL.filter (\e -> snd e /= 0) stats
 
   unless (DL.null nonzero) $
-    logInfo $ DL.intercalate ", " $ (\(n, i) -> n ++ ": " ++ show i) <$> nonzero
+    logInfoN $ T.pack $ DL.intercalate ", " $ (\(n, i) -> n <> ": " <> show i) <$> nonzero
 
   traverse_ sendMetric (mkTaggedMetrics "counters" stats)
   traverse_ sendMetric (mkNonTaggedMetrics stats)
