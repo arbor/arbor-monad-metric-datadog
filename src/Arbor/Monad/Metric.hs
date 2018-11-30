773bfe392e6226f7cc45d9df01e7d44bf7376d5e
John Ky <newhoggy@gmail.com>
2018-11-30 17:01:57 +1100

    Remove unused functions

--- src/Arbor/Monad/Metric.hs
+++ src/Arbor/Monad/Metric.hs
@@ -1,23 +1,18 @@
 {-# LANGUAGE DataKinds        #-}
-{-# LANGUAGE TypeApplications #-}
 
 module Arbor.Monad.Metric
   ( MonadMetrics
   , Z.getMetrics
 
   , newMetricsIO
-  , resetStats
   , valuesByKeys
   , extractValues
   ) where
 
 import Arbor.Monad.Metric.Type     (MetricFamily (..), MetricMap, Metrics (Metrics), MonadMetrics, getMetricMap)
 import Control.Concurrent.STM.TVar
-import Control.Lens
 import Control.Monad.IO.Class
 import Control.Monad.STM           (STM, atomically)
-import Data.Foldable
-import Data.Generics.Product.Any
 
 import qualified Arbor.Monad.Metric.Type as Z
 import qualified Control.Concurrent.STM  as STM
@@ -46,22 +41,3 @@ extractValues m = do
   let tvars = M.elems m
   nums <- sequence $ readTVar <$> tvars
   return (zip names nums, tvars)
-
-resetStats :: ()
-  => MonadMetrics m
-  => m ()
-resetStats = do
-  metrics   <- Z.getMetrics
-  counters  <- liftIO $ STM.readTVarIO $ metrics ^. the @"counters"
-  gauges    <- liftIO $ STM.readTVarIO $ metrics ^. the @"gauges"
-  setZeroes counters
-  setZeroes gauges
-
-setZeroes :: ()
-  => MonadIO m
-  => Num (MetricValue k)
-  => MetricMap k (MetricValue k)
-  -> m ()
-setZeroes cs = liftIO $ atomically $ do
-  (_, tvars) <- extractValues cs
-  traverse_ (`modifyTVar` const 0) tvars
