d06d76c81624766c07c53c4410b5538f7a52e45e
John Ky <newhoggy@gmail.com>
2018-12-06 11:41:49 +1100

    Give users control over tags

--- src/Arbor/Monad/Metric/Datadog.hs
+++ src/Arbor/Monad/Metric/Datadog.hs
@@ -7,57 +7,54 @@ module Arbor.Monad.Metric.Datadog
   , mkEvent
   ) where
 
-import Arbor.Monad.Metric.Type   (Counter (..), Gauge (..), MonadMetrics, getMetricMapTVar)
+import Arbor.Monad.Metric.Datadog.Internal
+import Arbor.Monad.Metric.Datadog.Internal.Show
+import Arbor.Monad.Metric.Type                  (Counter, Gauge, MonadMetrics, getMetricMapTVar)
 import Control.Lens
 import Control.Monad.IO.Class
 import Data.Foldable
 import Data.Generics.Product.Any
 import Data.Proxy
-import Data.Semigroup            ((<>))
+import Data.Semigroup                           ((<>))
 
 import qualified Arbor.Monad.Metric        as C
 import qualified Arbor.Network.StatsD      as S
 import qualified Arbor.Network.StatsD.Type as Z
 import qualified Control.Concurrent.STM    as STM
 import qualified Data.Map.Strict           as M
+import qualified Data.Set                  as S
 import qualified Data.Text                 as T
 
 logStats :: (S.MonadStats m, MonadMetrics m) => m ()
 logStats = do
   tCounterMap <- getMetricMapTVar
   (counters, _)  <- liftIO . STM.atomically $ STM.swapTVar tCounterMap M.empty >>= C.extractValues (Proxy @Counter)
-  traverse_ S.sendMetric $ mkMetricsCounterTagged "counters" counters
-  traverse_ S.sendMetric $ mkMetricsCounterNonTagged counters
+  traverse_ S.sendMetric $ mkMetricsCounter counters
 
   tGaugeMap <- getMetricMapTVar
   (gauge, _)  <- liftIO . STM.atomically $ STM.swapTVar tGaugeMap M.empty >>= C.extractValues (Proxy @Gauge)
-  traverse_ S.sendMetric $ mkMetricsGaugeTagged "gauge" gauge
-  traverse_ S.sendMetric $ mkMetricsGaugeNonTagged gauge
+  traverse_ S.sendMetric $ mkMetricsGauge gauge
 
 metricName :: String -> T.Text
 metricName n = T.replace " " "_" (T.pack n)
 
--- create metric m, but tag with stat:[actual stat name]
-mkMetricsGaugeTagged :: String -> [(Gauge, Double)] -> [Z.Metric]
-mkMetricsGaugeTagged m =
-  fmap (\(Gauge n, i) -> S.gauge (Z.MetricName (metricName m)) id i & the @"tags" %~ ([S.tag "stat" (T.pack n)] ++))
+-- create metrics for each counter
+mkMetricsGauge :: [(Gauge, Double)] -> [S.Metric]
+mkMetricsGauge = fmap (uncurry mkGauge)
+  where mkGauge :: Gauge -> Double -> S.Metric
+        mkGauge g v = S.gauge (Z.MetricName (metricName (T.unpack name))) id v & the @"tags" .~ (toStat <$> tags)
+          where name = g ^. the @"name"
+                tags = g ^. the @"tags" & S.toList
 
 -- create metrics for each counter
-mkMetricsGaugeNonTagged :: [(Gauge, Double)] -> [S.Metric]
-mkMetricsGaugeNonTagged =
-  fmap (\(Gauge n, i) -> S.gauge (Z.MetricName (metricName n)) id i)
-
--- create metric m, but tag with stat:[actual stat name]
-mkMetricsCounterTagged :: String -> [(Counter, Int)] -> [Z.Metric]
-mkMetricsCounterTagged m =
-  fmap (\(Counter n, i) -> S.addCounter (Z.MetricName (metricName m)) id i & the @"tags" %~ ([S.tag "stat" (T.pack n)] ++))
-
--- create metrics for each counter
-mkMetricsCounterNonTagged :: [(Counter, Int)] -> [S.Metric]
-mkMetricsCounterNonTagged =
-  fmap (\(Counter n, i) -> S.addCounter (Z.MetricName (metricName n)) id i)
+mkMetricsCounter :: [(Counter, Int)] -> [S.Metric]
+mkMetricsCounter = fmap (uncurry mkCounter)
+  where mkCounter :: Counter -> Int -> S.Metric
+        mkCounter g v = S.addCounter (Z.MetricName (metricName (T.unpack name))) id v & the @"tags" .~ (toStat <$> tags)
+          where name = g ^. the @"name"
+                tags = g ^. the @"tags" & S.toList
 
 mkEvent :: [(String, Int)] -> String -> Z.Tag -> String -> Z.Event
 mkEvent stats etitle etag fn = S.event (T.pack etitle) desc & the @"tags" %~ ([etag] ++)
   where desc = T.intercalate "\n" $ T.pack <$> ("File processed: " <> fn) : info
-        info = (\(n, i) -> n <> ": " <> show i) <$> stats
+        info = (\(n, i) -> n <> ": " <> showInt i) <$> stats
--- test/Arbor/Monad/Datadog/MetricSpec.hs
+++ test/Arbor/Monad/Datadog/MetricSpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications    #-}
 
@@ -6,16 +8,19 @@ module Arbor.Monad.Datadog.MetricSpec
   ) where
 
 import Control.Concurrent
-import Control.Exception      (bracket)
+import Control.Exception         (bracket)
+import Control.Lens
+import Control.Monad
 import Control.Monad.IO.Class
+import Data.Function
+import Data.Generics.Product.Any
 import Data.Proxy
-import Data.Semigroup         ((<>))
+import Data.Semigroup            ((<>))
 
 import qualified Arbor.Monad.Datadog.MetricApp as A
 import qualified Arbor.Monad.Datadog.UdpServer as UDP
 import qualified Arbor.Monad.Metric            as M
 import qualified Arbor.Monad.Metric.Datadog    as M
-import qualified Arbor.Monad.Metric.Type       as M
 import qualified Control.Concurrent.STM        as STM
 import qualified Data.ByteString.Char8         as BS
 import qualified Data.Map.Strict               as MAP
@@ -36,6 +41,9 @@ handler tMsgs addr msg = do
   STM.atomically $ STM.modifyTVar tMsgs (msg:)
   putStrLn $ "From " ++ show addr ++ ": " ++ show msg
 
+encodeMetrics :: [BS.ByteString] -> [BS.ByteString]
+encodeMetrics = (:[]) . mconcat . fmap (<> "\n")
+
 spec :: Spec
 spec = describe "Arbor.Monad.MetricSpec" $ do
   it "Metrics library actually sends statsd messages over UDP" $ requireTest $ do
@@ -43,13 +51,18 @@ spec = describe "Arbor.Monad.MetricSpec" $ do
     sock <- liftIO $ UDP.createUdpServer "5555"
     threadId <- liftIO $ forkIO $ UDP.runUdpServer sock (handler tMessages)
     liftIO $ threadDelay 1000000
-    let counterExpected = "MetricApp.counters:10|c|#stat:test.counter\nMetricApp.test.counter:10|c\n" :: BS.ByteString
-    let gaugeExpected = "MetricApp.gauge:20.000000|g|#stat:test.gauge\nMetricApp.test.gauge:20.000000|g\n" :: BS.ByteString
+    -- let counterExpected = "MetricApp.counters:10|c|#stat:test.counter\nMetricApp.test.counter:10|c\n" :: BS.ByteString
+    -- let gaugeExpected = "MetricApp.gauge:20.000000|g|#stat:test.gauge\nMetricApp.test.gauge:20.000000|g\n" :: BS.ByteString
     liftIO $ A.runMetricApp $ do
-      M.metric (M.Counter "test.counter") 10
-      M.metric (M.Gauge   "test.gauge"  ) 20
+      M.metric (M.counter "test.counter"                                        ) 10
+      M.metric (M.gauge   "test.gauge"                                          ) 20
+      M.metric (M.gauge   "test.gauge"  & the @"tags" .~ M.tags [("foo", "bar")]) 30
       M.logStats
     liftIO $ threadDelay 3000000
     liftIO $ killThread threadId
-    messages <- liftIO $ STM.readTVarIO tMessages
-    messages === [counterExpected <> gaugeExpected]
+    messages :: [BS.ByteString] <- liftIO $ STM.readTVarIO tMessages
+    mconcat (BS.lines <$> messages) ===
+      [ "MetricApp.test.counter:10|c"
+      , "MetricApp.test.gauge:20.000000|g"
+      , "MetricApp.test.gauge:30.000000|g|#foo:bar"
+      ]
